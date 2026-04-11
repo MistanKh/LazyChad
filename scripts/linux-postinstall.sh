4c327bbd70e5f218cb5ac325a70813dc49327918
MistanKh <mistankhomdram@gmail.com>
2026-04-11 16:02:35 +0530

    feat: add multi-distro packaging support (.deb, .rpm) and universal dependency installer

---
+++ scripts/linux-postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Post-installation script for Debian/RPM packages
+
+echo "==> LazyChad has been successfully installed."
+echo "==> To complete the setup (Node, Python, Rust providers), please run:"
+echo "    lazychad-deps"
+echo ""
+echo "==> Then start the editor with: lchad"
