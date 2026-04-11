01d08d31e3125a0474192754dbd538aae9878780
MistanKh <mistankhomdram@gmail.com>
2026-04-12 01:07:57 +0530

    fix: improve config copy logic and force package.path for local modules

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
+-- Ensure local modules are in the search path
+package.path = package.path .. ";" .. vim.fn.stdpath "config" .. "/lua/?.lua"
+package.path = package.path .. ";" .. vim.fn.stdpath "config" .. "/lua/?/init.lua"
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
